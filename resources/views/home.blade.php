92eb063960070935d105f40fb7196795804468d2
Osbaldo Toledo <osbaldo.toledoramos1995@gmail.com>
2022-04-11 15:25:41 -0500

    WorkInProgress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,6 @@
 <!-- Carrusel -->
 <div class="bg-black">
     <div class="container">
-        <p>
-            Update now
-        </p>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
